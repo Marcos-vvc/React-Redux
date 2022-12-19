dc492744ec5c0ef2aa4f8be46a39d02080264c6e
marcos <marcos-dido@hotmail.com>
2022-12-19 19:35:00 -0300

    atualizando nomes

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -2,10 +2,10 @@ import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import {
   getCounties,
-  getDistricts,  
+  getDistricts,
   getStates,
   selectApp,
-} from "../../reducers/estados";
+} from "../../reducers/states";
 import { AppDispatch, useAppSelector } from "../../store";
 
 const Home = () => {
@@ -36,11 +36,14 @@ const Home = () => {
     setSelectedMu(event.target.value);
   };
 
-  return (    
+  return (
     <div className="flex h-screen ">
       <div className="flex flex-col justify-center m-auto gap-2 border-double border-4 h-2/4 w-2/5 items-center bg-gray-500">
-      <h1 className="text-xl">Selecione os Estados</h1>
-        <select onChange={handleSelectedUf} className =" border-2 rounded-md w-2/4 cursor-pointer	">
+        <h1 className="text-xl">Selecione os Estados</h1>
+        <select
+          onChange={handleSelectedUf}
+          className=" border-2 rounded-md w-2/4 cursor-pointer	"
+        >
           <option>Selecione o Estado</option>
           {(states ?? []).map((uf) => (
             <option key={uf.id} value={uf.id}>
@@ -49,7 +52,10 @@ const Home = () => {
           ))}
         </select>
 
-        <select onChange={handleCounties} className ="border-2 rounded-md w-2/4	cursor-pointer	">
+        <select
+          onChange={handleCounties}
+          className="border-2 rounded-md w-2/4	cursor-pointer	"
+        >
           <option value="0">Selecione o Municipio</option>
           {(counties ?? []).map((city) => (
             <option key={city.id} value={city.id}>
@@ -58,7 +64,7 @@ const Home = () => {
           ))}
         </select>
 
-        <select className ="border-2 rounded-md w-2/4	cursor-pointer	">
+        <select className="border-2 rounded-md w-2/4	cursor-pointer	">
           <option value="0">Selecione a Região Administrativa</option>
           {(districts ?? []).map((district) => (
             <option key={district.id} value={district.id}>
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useSelector } from "react-redux";
-import  allActionsSlice  from "../reducers/estados";
-
+import allActionsSlice from "../reducers/states";
 
 export const store = configureStore({
-  reducer: allActionsSlice 
-  
+  reducer: allActionsSlice,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
@@ -13,54 +11,3 @@ export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
